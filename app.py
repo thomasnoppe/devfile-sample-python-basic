082dac0b9c4470b768309d8ca53da1218555c4d0
thomasnoppe <49635320+thomasnoppe@users.noreply.github.com>
2022-05-09 09:22:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    ip_address = flask.request.remote_addr
+    return "Requester IP: " + ip_address
 
 if __name__ == '__main__':
     serve(app, host='0.0.0.0', port=8080)
